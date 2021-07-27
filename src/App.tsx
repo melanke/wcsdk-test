5f7c6d47b1c31cf0ff83acabf782c3b42d1d388a
Gil Lopes Bueno <melanke.hc@gmail.com>
2021-07-27 18:38:30 -0300

    commit of the problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useWalletConnect} from "@cityofzion/wallet-connect-sdk-react";
 
 function App() {
+  const walletConnectCtx = useWalletConnect()
+
+  const connect = async () => {
+    await walletConnectCtx.connect()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {walletConnectCtx.loadingSession
+        ? "Loading Session"
+        : !walletConnectCtx.session ? <button
+            onClick={connect}>Connect your Wallet</button>
+        : "Connected"
+      }
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {WalletConnectContextProvider} from '@cityofzion/wallet-connect-sdk-react';
+
+const wcOptions = {
+  chainId: "neo3:testnet", // blockchain and network identifier
+  logger: "debug", // use debug to show all log information on browser console
+  methods: ["invokefunction"], // which RPC methods do you plan to call
+  relayServer: "wss://connect.coz.io", // which relay server do you want to use, alternatively you can use "wss://relay.walletconnect.org"
+  appMetadata: {
+    name: "MyApplicationName", // your application name to be displayed on the wallet
+    description: "My Application description", // description to be shown on the wallet
+    url: "https://myapplicationdescription.app/", // url to be linked on the wallet
+    icons: ["https://myapplicationdescription.app/myappicon.png"], // icon to be shown on the wallet
+  }
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WalletConnectContextProvider options={wcOptions}>
+      <App />
+    </WalletConnectContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
